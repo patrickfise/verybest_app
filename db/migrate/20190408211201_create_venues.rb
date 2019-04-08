57294eca89d3adbedfe859f4203a62f68fed7740
Raghu Betina <raghu@firstdraft.com>
2019-04-08 21:12:01 +0000

    Generate venues resource with draft:resource

---
+++ db/migrate/20190408211201_create_venues.rb
@@ -0,0 +1,11 @@
+class CreateVenues < ActiveRecord::Migration[5.1]
+  def change
+    create_table :venues do |t|
+      t.string :address
+      t.integer :neighborhood_id
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
